b3a8cbe593e4ec8856928356a14da0a46f634353
Kuzmin Mikhail <nigol@list.ru>
2020-11-09 08:48:39 +0300

    start project

---
+++ src/test/java/com/MyProject/MyProject/MyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyProject.MyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
